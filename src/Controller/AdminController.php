5fa0bbc5382871e0a8ac6370f4a6a8b08a4cc1f0
unknown <your_address_email@domain.com>
2021-04-08 18:50:52 +0200

    correction

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,7 +10,6 @@ class AdminController extends AbstractController
 {
     /**
      * @Route("/a/admin", name="admin")
-     * @IsGranted("ROLE_ADMIN")
      */
     public function index(): Response
     {
